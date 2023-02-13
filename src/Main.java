52866dfcb32ed75e2cd4d1bf0cac0bd01647ec06
Sidiki CAMARA <absicamara@gmail.com>
2023-02-13 02:45:29 +0000

    Complete App4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import entity.Etudiant;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+            List<Etudiant> etudiants = new ArrayList<Etudiant>();
 
-    }
+        etudiants.add(new Etudiant("CAMARA", "Sidiki", 'M', 35, 3, "ISI"));
+        etudiants.add(new Etudiant("Diallo", "Binta", 'F', 65, 33, "SSI"));
+
+
+            try {
+
+                System.out.println(etudiants.get(0).getPrenom());
+                System.out.println(etudiants.get(1).getPrenom());
+                System.out.println(etudiants.get(2).getPrenom());
+
+
+            }catch (NullPointerException  e){
+//            MANAGE ERROR HERE
+                System.err.println(e.getMessage());
+
+            }    }
+
 }
