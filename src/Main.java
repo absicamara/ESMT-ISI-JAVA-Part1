f615c1590123857808c0f302a71c97c7f3f89bb6
Sidiki CAMARA <absicamara@gmail.com>
2023-02-13 02:27:50 +0000

    complete App3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Etudiant etudiant = new Etudiant("CAMARA", "Sidiki", 'M', 30, 5555, "MP-ISI");
-        etudiant.sePresenter();    }
+
+    }
 }
---
+++ src/entity/EtudiantSurveillant.java
@@ -0,0 +1,14 @@
+package entity;
+import interfaces.Surveiller;
+
+public class EtudiantSurveillant extends Etudiant implements Surveiller {
+
+    public EtudiantSurveillant(String nom, String prenom, char sexe, int age, int matricule, String formation) {
+        super(nom, prenom, sexe, age, matricule, formation);
+    }
+
+    @Override
+    public void surveillerEvaluation() {
+
+    }
+}
