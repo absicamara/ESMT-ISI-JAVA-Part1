fdaa81696db1762cf8a557e9e4cbe4f63b5f4e0f
Sidiki CAMARA <absicamara@gmail.com>
2023-02-14 07:27:42 +0000

    Completed App12 with Observer pattern

--- Application11/src/main/java/Observer/Observer.java
+++
@@ -1,9 +0,0 @@
-package Observer;
-
-import Subject.FenetrePrincipale;
-
-public abstract class Observer {
-
-    protected FenetrePrincipale fenetrePrincipale;
-    public abstract void update();
-}
--- Application11/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.sidiki.application11 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.sidiki.application11 to javafx.fxml;
-    exports com.sidiki.application11;
-}
